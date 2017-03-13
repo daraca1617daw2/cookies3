98e4ad1bd19622b85b6ffde7563fd6d2de15e100
davrami <david.rami007@gmail.com>
2017-03-13 21:01:09 +0100

    cookies3 commit 2

--- cookies3.php
+++ cookies3.php
@@ -6,9 +6,9 @@
     <body> 
         <?php 
             echo "Afegint Cookies<br>"; 
-            echo "Sóc el desenvolupador daraca1617daw2"; 
-            setcookie("nom", "Jaume Pons", time()+3600, "/", "", 0); 
-            setcookie("edat", "20", time()+3600, "/", "", 0); 
+            echo "Sóc el desenvolupador daraca1617daw2";
+setcookie("nom", "david", time()+3600, "/", "", 0);
+setcookie("edat", "20", time()+3600, "/", "", 0); 
             setcookie("cicle", "DAW", time()+3600, "/", "", 0); 
             setcookie("curs", "2n", time()+3600, "/", "", 0); 
             setcookie("any", "2016­2017", time()+3600, "/", "", 0);            
